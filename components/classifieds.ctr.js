85a4d978233718e415ccc29b0b4f36fd72119e5b
lucas <lucas.nandan@gmail.com>
2018-07-16 16:55:46 +0200

    ajout admin delete search

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -3,11 +3,17 @@
 
   angular
     .module("ngClassifieds")
-    .controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav) {
+    .controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast, $mdDialog) {
             
       classifiedsFactory.getClassifieds().then(function(classifieds) {
           $scope.classifieds = classifieds.data;
         });
+
+        var contact = {
+          name:"ryan chenkie",
+          phone: "(555) 555-5555",
+          email: "oalala@gmail.com"
+        };
     
         $scope.openSidebar = function() {
           $mdSidenav('left').open();
@@ -16,7 +22,57 @@
         $scope.closeSidebar = function() {
           $mdSidenav('left').close();
         };
-      
-      });
+        
+        $scope.saveClassified = function(classified) {
+          if(classified) {
+            classified.contact = contact;
+            $scope.classifieds.push(classified);
+            $scope.classified = {};
+            $scope.closeSidebar();
+            showToast("Classified saved!");
+          } 
+        };
 
+        $scope.editClassified = function(classified) {
+          $scope.editing = true;
+          $scope.openSidebar();
+          $scope.classified = classified;
+        };
+
+        $scope.saveEdit = function() {
+          $scope.editing = false;
+          $scope.classified = {};
+          $scope.closeSidebar();
+          showToast("Edit saved!");
+        };
+
+        $scope.deleteClassified = function(event, classified) {
+          var confirm = $mdDialog.confirm()
+            .title('Are you sure you want to delete '+ classified.title + '?')
+            .ok('Yes')
+            .cancel('No')
+            .targetEvent(event);
+          $mdDialog.show(confirm).then(function() {
+            var index = $scope.classifieds.indexOf(classified);
+            $scope.classifieds.splice(index, 1);
+          }, function() {
+            
+          });
+        };
+
+        function showToast(message) {
+          $mdToast.show(
+            $mdToast.simple()
+              .content(message)
+                .position('top, right')
+                .hideDelay(3000)
+          );
+        }
+
+        $scope.newClassified = function() {
+          $scope.openSidebar();
+          $scope.editing = false;
+          $scope.classified = {};
+        };
+      });
 })();
